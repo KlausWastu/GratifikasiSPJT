3d0633d0fdb9c9f44b39b8abbbd052de275725ed
Klaus Rajendra Wastu <wastuklaus28@gmail.com>
2023-07-20 14:09:09 +0700

    Menambahkan router downloadPDF untuk laporpermintaan beserta fungsinya

--- app/unitpengendaligratifikasi/riwayatreview/permintaan/router.js
+++ app/unitpengendaligratifikasi/riwayatreview/permintaan/router.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
-const { index } = require("./controller");
+const { index, downloadPDFView } = require("./controller");
 
 const { isLogin, UPGAkses } = require("../../../middleware/auth");
 
 /* GET home page. */
 router.use(isLogin);
 router.get("/", UPGAkses, index);
+router.get("/down/:id", UPGAkses, downloadPDFView);
 
 module.exports = router;
